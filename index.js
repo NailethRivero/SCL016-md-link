cd633c91103e92420a740b39878e4072513f5613
unknown <nailethhdarina@gmail.com>
2021-06-15 08:36:48 -0400

    updating functions and taking out functions that do not belong here

--- index.js
+++ index.js
@@ -107,57 +107,8 @@ const validateLinks = (arrLinks) => {
   return Promise.all(arrPromises);
 };
 
-const pathPrueba = './TEST.md';
+/*mdLinks("./TEST.md", { validate: true }).then((res) => {
+  console.log(res);
+});*/
 
-//links
-mdLinks(pathPrueba, { validate: false })
-  .then(console.log)
-  .catch(console.error);
-
-// validate
-mdLinks(pathPrueba, { validate: true })
-  .then(console.log)
-  .catch(console.error);
-
-//stats
-mdLinks(pathPrueba, { validate: false })
-  .then((resp) => {
-    const statsLinks = getStats(resp);
-    console.log(chalk.hex('8BDCD6').underline('Stats'));
-    console.log(`Total: ${statsLinks.Total}. Unique: ${statsLinks.Unique}.`);
-    console.table(statsLinks);
-  })
-  .catch(console.error);
-
-// Stats + validate
-mdLinks(pathPrueba, { validate: true })
-  .then((resp) => {
-    const statsLinksValidate = getStatsValidate(resp);
-    console.log(chalk.hex('8BDCD6').italic('Stats + Validate'));
- 
-    console.table(statsLinksValidate);
-  })
-  .catch(console.error);
-
-//Stats Links (Unique + Total)
-const getStats = (arrLinks) => {
-  const totalLinks = arrLinks.length;
-  const linksUniqueArray = [...new Set(arrLinks.map((link) => link.href))];
-
-  return { Total: totalLinks, Unique: linksUniqueArray.length };
-};
-
-//Stats + Validate Links (Unique + Total + broken)
-
-const getStatsValidate = (arrLinksValidate) => {
-  const totalLinks = arrLinksValidate.length;
-  const linksUniqueArray = [...new Set(arrLinksValidate.map((link) => link.href))];
-  const failedLinks = arrLinksValidate.filter((link) => link.statusText !== 'OK');
-
-  return { Total: totalLinks, Unique: linksUniqueArray.length, Broken: failedLinks.length };
-};
-  module.exports = {
-    getStats,
-    getStatsValidate,
-    mdLinks,
-  };
+  module.exports = mdLinks
