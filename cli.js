c24dac10a0e69ce7673ec65ad20f59326699b6d3
unknown <nailethhdarina@gmail.com>
2021-06-16 09:39:32 -0400

    adding functions to show stats and validate

--- cli.js
+++ cli.js
@@ -10,33 +10,6 @@ program
 program.parse(process.argv);
 const path = process.argv[2];
 
-//validate
-if (program.validate && !program.stats) {
-	mdLinks(path, { validate: true })
-	  .then(console.log)
-	  .catch(console.error);
-  }
-
-//Stats
-if (!program.validate && program.stats) {
-	mdLinks(path, { validate: false })
-	  .then((resp) => {
-		const statsLinks = getStats(resp);
-		console.table(statsLinks);
-	  })
-	  .catch(console.error);
-  };
-
-//validate & stats
-if (program.validate && program.stats) {
-  mdLinks(path, { validate: true })
-    .then((resp) => {
-      const statsLinksValidate = getStatsValidate(resp);
-      console.table(statsLinksValidate);
-    })
-    .catch(console.error);
-};
-
 //Stats Links (Unique & Total)
 const getStats = (arrLinks) => {
 	const totalLinks = arrLinks.length;
@@ -54,21 +27,50 @@ const getStats = (arrLinks) => {
   
 	return { Total: totalLinks, Unique: linksUniqueArray.length, Broken: failedLinks.length };
   };
+
+//validate
+if (program.validate && !program.stats) {
+	mdLinks(path, { validate: true })
+	  .then(console.log)
+	  .catch(console.error);
+  }
+
+//Stats
+ else if (!program.validate && program.stats) {
+	mdLinks(path, { validate: false })
+	  .then((resp) => {
+		const statsLinks = getStats(resp);
+		console.table(statsLinks);
+	  })
+	  .catch(console.error);
+  }
+
+//validate & stats
+else if (program.validate && program.stats) {
+  mdLinks(path, { validate: true })
+    .then((resp) => {
+      const statsLinksValidate = getStatsValidate(resp);
+      console.table(statsLinksValidate);
+    })
+    .catch(console.error);
+};
+
+
  
-  const pathPrueba = './TEST.md';
+  const testingPath = './TEST.md';
 
   //links
-  mdLinks(pathPrueba, { validate: false })
+  mdLinks(testingPath, { validate: false })
 	.then(console.log)
 	.catch(console.error);
   
   // validate
-  mdLinks(pathPrueba, { validate: true })
+  mdLinks(testingPath, { validate: true })
 	.then(console.log)
 	.catch(console.error);
   
   //stats
-  mdLinks(pathPrueba, { validate: false })
+  mdLinks(testingPath, { validate: false })
 	.then((resp) => {
 	  const statsLinks = getStats(resp);
 	  console.log(chalk.hex('8BDCD6').underline('Stats'));
@@ -78,7 +80,7 @@ const getStats = (arrLinks) => {
 	.catch(console.error);
   
   // Stats + validate
-  mdLinks(pathPrueba, { validate: true })
+  mdLinks(testingPath, { validate: true })
 	.then((resp) => {
 	  const statsLinksValidate = getStatsValidate(resp);
 	  console.log(chalk.hex('8BDCD6').italic('Stats + Validate'));
@@ -86,5 +88,3 @@ const getStats = (arrLinks) => {
 	  console.table(statsLinksValidate);
 	})
 	.catch(console.error);
-  
-
